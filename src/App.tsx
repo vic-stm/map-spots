a017983d21b3442acfc7dc9a9c231167013bffef
Mikel Sanchez <mikel.palazuelo@rindus.de>
2025-04-23 22:04:38 +0200

    feat: integrate Tailwind CSS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <h1>Starting project</h1>;
+  return (
+    <main className="min-h-screen bg-blue-900 flex items-start justify-center">
+      <h1 className="text-white text-5xl m-6 font-bold">Starting project!!</h1>
+    </main>
+  );
 }
 
 export default App;
